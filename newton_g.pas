cb6e324402620f43267866fb2304583ef2ca883a
ABO BAKR ALMOSTAFA <abubaker_ms1@hotmail.com>
2025-01-19 22:24:09 +0300

    from local

---
+++ newton_g.pas
@@ -0,0 +1,147 @@
+program newton_gaus;
+
+uses wincrt;
+
+const maxn=3;
+
+label start;
+type matrix=array[1..maxn,1..maxn] of real;
+     vector=array[1..maxn] of real;
+
+var  Jak:matrix;
+     U,S,DS:vector;
+     a,b,c,d,e,f,g,h,i,j:array[1..3] of integer;
+     k:1..3;
+     L,st,step:integer;
+
+function Fxyz(x,y,z:real;k:byte):real;
+begin
+ Fxyz:=a[k]*x*sqr(x)+b[k]*y*sqr(y)+c[k]*z*sqr(z)+d[k]*sqr(x)+e[k]*sqr(y)+f[k]*sqr(z)+g[k]*x+h[k]*y+i[k]*z+j[k];
+end;
+
+function derive(a,b,c:integer;x:real):real;
+begin
+ derive:=3*a*sqr(x)+2*b*x+c;
+end;
+
+Procedure Gaus(oparray:matrix;frvec:vector;n:byte;var X:vector);
+
+type expanded=array[1..maxn,1..maxn+1,0..maxn] of real;
+     GausArray=array[1..maxn,1..maxn+1] of real;
+
+var  A:expanded; B:GausArray;
+     i,j,k:0..maxn;
+     tempsum:real;
+
+begin
+
+ for i:=1 to n do
+ begin
+
+  for j:=1 to n do
+   A[i,j,0]:=OpArray[i,j];
+  A[i,n+1,0]:=FrVec[i];
+
+ end;
+
+ for k:=1 to n do
+ begin
+
+  for j:=k to n+1 do
+   B[k,j]:=A[k,j,k-1]/A[k,k,k-1];
+
+  for i:=k+1 to n do
+   for j:=k to n+1 do
+      a[i,j,k]:=a[i,j,k-1]-a[i,k,k-1]*B[k,j];
+
+ end;
+
+ X[n]:=B[n,n+1];
+ for i:=n-1 downto 1 do
+ begin
+  tempsum:=0;
+  for j:=i+1 to n do
+   tempsum:=tempsum+X[j]*B[i,j];
+  X[i]:=B[i,n+1]-tempsum;
+ end;
+end;
+
+BEGIN {main program}
+ start:writeln('        solving nonlinear equations using advanced Newton-Gauss method.');
+ writeln('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+ for k:=1 to maxn do
+ begin
+ a[k]:=0;b[k]:=0;c[k]:=0;d[k]:=0;e[k]:=0;f[k]:=0;g[k]:=0;h[k]:=0;i[k]:=0;j[k]:=0;
+ end;
+ writeln('what kind of equations do you want to solve?');
+ writeln('1:linear');writeln('2:second degree');writeln('3:third degree');writeln('0:get out');
+ readln(L);
+ case l of
+   1:begin
+      for k:=1 to maxn do
+       begin
+         writeln('input the operators of U',k,'=aX+bY+cZ+d');
+         write('a',k,'=');read(jak[k,1]);write('b',k,'=');read(jak[k,2]);
+         write('c',k,'=');read(jak[k,3]);write('d',k,'=');read(u[k]);
+       end;
+      Gaus(Jak,U,3,DS);
+      writeln('X=',DS[1]:0:7);
+      writeln('Y=',DS[2]:0:7);
+      writeln('Z=',DS[3]:0:7);
+      goto start;
+     end;
+     
+   2:for k:=1 to maxn do
+     begin
+         writeln('input the operators of U',k,'=aX^2+bY^2+cZ^2+dX+eY+fZ+g');
+         write('a',k,'=');read(d[k]);write('b',k,'=');read(e[k]);write('c',k,'=');read(f[k]);write('d',k,'=');
+         read(g[k]);write('e',k,'=');read(h[k]);write('f',k,'=');read(i[k]);write('g',k,'=');read(j[k]);
+     end;
+
+
+   3:for k:=1 to maxn do
+     begin
+        writeln('input the operators of U',k,'=aX^3+bY^3+cZ^3+dX^2+eY^2+fZ^2+gX+hY+iZ+j');
+        write('a',k,'=');read(a[k]);write('b',k,'=');read(b[k]);write('c',k,'=');read(c[k]);write('d',k,'=');read(d[k]);
+        write('e',k,'=');read(e[k]);write('f',k,'=');read(f[k]);write('g',k,'=');read(g[k]);write('h',k,'=');read(h[k]);
+        write('i',k,'=');read(i[k]);write('j',k,'=');read(j[k]);
+     end;
+   4:for k:=1 to maxn do
+     begin
+        writeln('input the operators of U',k,'=aX^3+bY^3+cZ^3+dX^2+eY^2+fZ^2+gX+hY+iZ+j');
+        write('a',k,'=');read(a[k]);write('b',k,'=');read(b[k]);write('c',k,'=');read(c[k]);write('d',k,'=');read(d[k]);
+        write('e',k,'=');read(e[k]);write('f',k,'=');read(f[k]);write('g',k,'=');read(g[k]);write('h',k,'=');read(h[k]);
+        write('i',k,'=');read(i[k]);write('j',k,'=');read(j[k]);
+     end;
+
+   0:halt;
+ end;
+ readln;
+ writeln(' please input first step X0,Y0,Z0 ');
+ write('X',0,'=');read(s[1]);
+ write('Y',0,'=');read(s[2]);
+ write('Z',0,'=');read(s[3]);
+
+ writeln('input number of steps please');
+ readln(step);
+
+ for st:=1 to step do
+ begin
+
+  for k:=1 to maxn do
+  begin
+   U[k]:=-Fxyz(s[1],s[2],s[3],k);
+   Jak[k,1]:=derive(a[k],d[k],g[k],s[1]);
+   Jak[k,2]:=derive(b[k],e[k],h[k],s[2]);
+   Jak[k,3]:=derive(c[k],f[k],i[k],s[3]);
+  end;
+  Gaus(Jak,U,3,DS);
+  for k:=1 to maxn do
+     S[k]:=S[k]+DS[k];
+   writeln('X',st,'=',S[1]:0:7);
+   writeln('Y',st,'=',S[2]:0:7);
+   writeln('Z',st,'=',S[3]:0:7);
+   writeln;
+ end;
+ goto start;
+END.
---
+++ oler-hwe.pas
@@ -0,0 +1,76 @@
+program oler_hween;
+
+uses wincrt;
+
+label start;
+
+const maxn=10; maxk=3;
+
+
+var a,b,c,d,e,f,g,h,u,v:real;
+    i,j,k,n:integer;
+    x:array[0..maxn] of real;
+    y:array[0..maxn,0..maxk] of real;
+    L:0..3;
+
+
+function fxy (x,y:real):real;
+begin
+fxy:=a*x*sqr(x)+b*y*sqr(y)+c*sqr(x)+d*sqr(y)+e*x+f*y+g;
+end;
+
+
+
+
+Begin
+start:writeln('        solving differential equation using Oler-Hween method.');
+writeln('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+writeln('y`=f(x,y)');
+writeln('what kind of equations do you want to solve?');
+writeln('1:linear');writeln('2:second degree');writeln('3:third degree');writeln('0:get out');
+readln(L);
+a:=0;b:=0;c:=0;d:=0;e:=0;f:=0;g:=0;
+ case l of
+    1:begin
+      writeln('y`=aX+bY+c');
+      writeln('Input a,b,c');
+      write('a=');read(e);write('b=');read(f);write('c=');read(g);
+     end;
+   2:begin
+          writeln('y`=aX^2+bY^2+cX+dY+e');
+          writeln('Input a,b,c,d,e please');
+          write('a=');read(c);write('b=');read(d);write('c=');read(e);write('d=');read(f);write('e=');read(g);
+
+
+     end;
+   3:begin
+          writeln('y`=aX^3+bY^3+cX^2+dY^2+eX+fY+g');
+          writeln('Input a,b,c,d,e,f,g please');
+          write('a=');read(a);write('b=');read(b);write('c=');read(c);
+          write('d=');read(d);write('e=');read(e);write('f=');read(f);write('g=');read(g);
+
+     end;
+  0:halt;
+ end;
+writeln('X is in [u,v].Input u,v please');
+write('u=');read(u);write('v=');read(v);
+writeln('input number of steps n');
+readln(n);
+h:=(v-u)/n;
+for i:=0 to n do
+ x[i]:=u+i*h;
+writeln('Input Y(X0) please');
+readln(y[0,0]);
+writeln('Finally please input number of improving steps k');
+readln(k);
+y[0,k]:=y[0,0];
+for i:=0 to n-1 do
+begin
+ y[i+1,0]:=y[i,k]+h*fxy(x[i],y[i,k]);
+ for j:=0 to k do
+  y[i+1,j+1]:=y[i,k]+(h/2)*(fxy(x[i],y[i,k])+fxy(x[i+1],y[i+1,j]));
+
+ writeln('y',i+1,'=',y[i+1,k+1]:0:9);
+end;
+goto start;
+End.
